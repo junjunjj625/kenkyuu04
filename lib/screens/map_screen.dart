c44d94603b90f8d97d7018256ae6add13763305d
梅原稜 <region.pc01@gmail.com>
2023-12-19 13:29:45 +0900

    Chapter06

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({
@@ -12,6 +13,17 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
 
+  @override
+  void initState() {
+    //位置情報が許可されていない時に許可をリクエストする
+    Future(() async {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        await Geolocator.requestPermission();
+      }
+    });
+    super.initState();
+  }
   final CameraPosition initialCameraPosition = const CameraPosition(
       target: LatLng(35.681236, 139.767125), // 東京駅
       zoom: 16.0,
