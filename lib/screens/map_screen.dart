79dcdd5cad83e6836795e33fceae30e8f5f4cfdb
梅原稜 <region.pc01@gmail.com>
2023-12-19 14:14:37 +0900

    Chapter08

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({
@@ -8,28 +8,16 @@ class MapScreen extends StatefulWidget {
   });
 
   @override
-  State<MapScreen> createState() => _MapScreenState();
+  _MapScreenState createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
 
-  final CameraPosition initialCameraPosition = const CameraPosition(
-    target: LatLng(35.681236, 139.767125), // 東京駅
+  final CameraPosition initialCameraPosition = CameraPosition(
+    target: LatLng(35.681236, 139.767125),
     zoom: 16.0,
   );
-
-  @override
-  void initState() {
-    //位置情報が許可されていない時に許可をリクエストする
-    Future(() async {
-      LocationPermission permission = await Geolocator.checkPermission();
-      if (permission == LocationPermission.denied) {
-        await Geolocator.requestPermission();
-      }
-    });
-    super.initState();
-  }
 
   @override
   void dispose() {
@@ -40,37 +28,45 @@ class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Map'),
-      ),
       body: GoogleMap(
         initialCameraPosition: initialCameraPosition,
-        onMapCreated: (GoogleMapController controller) {
+        onMapCreated: (GoogleMapController controller) async {
           mapController = controller;
+          await _requestPermission();
+          await _moveToCurrentLocation();
         },
         myLocationEnabled: true,
         myLocationButtonEnabled: false,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          // 現在地を取得
-          final Position position = await Geolocator.getCurrentPosition(
-            desiredAccuracy: LocationAccuracy.high,
-          );
-
-          // 現在地を中心にカメラを移動
-          mapController.animateCamera(
-            CameraUpdate.newCameraPosition(
-              CameraPosition(
-                target: LatLng(position.latitude, position.longitude),
-                zoom: 16.0,
-              ),
-            ),
-          );
-        },
-        tooltip: 'current position',
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+
+  Future<void> _requestPermission() async {
+    // 位置情報の許可を求める
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      await Geolocator.requestPermission();
+    }
+  }
+
+  Future<void> _moveToCurrentLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.always ||
+        permission == LocationPermission.whileInUse) {
+      // 現在地を取得
+      final Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      // 現在地にカメラを移動
+      await mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(position.latitude, position.longitude),
+            zoom: 16.0,
+          ),
+        ),
+      );
+    }
   }
 }
